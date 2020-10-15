3ef3dcfc87d9d795b4afd0cfd7fe7e869b1413d3
Jonathan Nguyen <jn04137@georgiasouthern.edu>
2020-10-15 15:03:44 -0400

    Got the hashmap for transition functions to work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use std::collections::HashMap;
 
 fn vec_format(text: &str) -> Vec<&str> {
     let string: &str = text.trim().trim_matches(|c| c== '}' || c=='{');
@@ -16,38 +17,44 @@ fn main() -> std::io::Result<()> {
     let mut text = String::new();
     buf_reader.read_line(&mut text)?;
     let alphabet: Vec<&str> = vec_format(&text); 
-    for x in alphabet {
-        print!("{} ", x);
-    };
-    text.clear();
-
-    println!();
     
     // Gets the states
     let mut text = String::new();
     buf_reader.read_line(&mut text)?;
     let states: Vec<&str> = vec_format(&text);
-    for x in states{
-        print!("{} ", x);
-    };
-    text.clear();
-
-    println!();
 
     // Gets the start state
     let mut text = String::new();
     buf_reader.read_line(&mut text)?;
-    let fin_state = text;
-    print!("{}", fin_state);
+    let start_state = text;
    
     // Gets the accept states
     let mut text = String::new(); // new text var is necessary here b/c prev var was owned by prev func
     buf_reader.read_line(&mut text)?;
     let accept_states: Vec<&str> = vec_format(&text);
-    for x in accept_states {
-        print!("{} ", x);
-    };
-    text.clear();
+
+    let mut transitive_functions: HashMap<String, char> = HashMap::new();
+    
+    let mut text = String::new();
+    let mut bytes = buf_reader.read_line(&mut text)?;
+    while bytes != 0 {
+        let state = text.chars().nth(1).unwrap();
+        let input = text.chars().nth(3).unwrap();
+        let next_state = text.chars().nth(7).unwrap();
+
+        let mut key = String::new();
+        
+        key.push(state);
+        key.push(input);
+
+        transitive_functions.insert(key, next_state);
+        text.clear();
+        bytes = buf_reader.read_line(&mut text)?;
+    }
+
+    for (key, val) in transitive_functions.iter() {
+        println!("key: {}, val: {}", key, val);
+    }
 
     Ok(())
 }
